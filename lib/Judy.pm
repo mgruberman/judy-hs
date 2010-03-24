8aca5f37ae1e09b753db35d3d0ff0c345e815789
Josh ben Jore <jjore@cpan.org>
2010-03-24 16:00:19 -0700

    Add comments

--- lib/Judy.pm
+++ lib/Judy.pm
@@ -14,9 +14,21 @@ use Sub::Exporter -setup => {
     exports => [qw[ PJERR JLAP_INVALID ]]
 };
 
-# Load all the interfaces. The XS package already loaded all the real
-# implementation anyway. Beware, each implementation also uses
-# Judy.pm.
+# Beware, each implementation also uses Judy.pm. Both Judy.pm and 1.pm
+# (for example) require() each other. In execution order:
+#
+# Compile Judy.pm
+# iz $INC{'Judy.pm'} true naow?
+# Execute Judy.pm
+# $INC{'Judy.pm'} iz true naow.
+# require 1.pm
+# Compile 1.pm
+# Execute 1.pm
+# require Judy.pm # but $INC{'Judy.pm'} is already true
+
+# Load the functional interfaces. The XS package already loaded all
+# the real implementation anyway.
+#
 require Judy::1;
 require Judy::L;
 require Judy::SL;
